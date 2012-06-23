40ffe0639931dfb98280b7cdeec16006a490536d
Ido Reis <idor@ti.com>
2012-06-24 01:33:23 +0300

    update Android.mk with mc firmware

--- Android.mk
+++ Android.mk
@@ -2,6 +2,14 @@ local_target_dir := $(TARGET_OUT)/etc/firmware/ti-connectivity
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := wl18xx-fw-mc.bin
+LOCAL_MODULE_TAGS := debug
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := FIRMWARE
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := wl18xx-fw.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
