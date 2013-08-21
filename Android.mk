7366d3ab02c564afdee07bb15da6af0e1a88c2ec
Yaniv Machani <yanivma@ti.com>
2013-08-21 13:17:39 +0300

    Cleaned redundant FW files

--- Android.mk
+++ Android.mk
@@ -2,41 +2,10 @@ local_target_dir := $(TARGET_OUT)/etc/firmware/ti-connectivity
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl18xx-fw-mc.bin
+LOCAL_MODULE := wl18xx-fw-2.bin
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl18xx-fw.bin
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl18xx-fw.ili
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl18xx-fw-pg1.1.bin
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl18xx-fw-pg1.1.ili
-LOCAL_MODULE_TAGS := debug
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
-include $(BUILD_PREBUILT)
